56a43990f2ebf83b20057958d364969cf011ccd6
Jesus Estevez <jesestrod1@gmail.com>
2018-12-22 21:14:04 +0100

    dashboard views added

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
+Route::resource('tasks', 'TaskController');
